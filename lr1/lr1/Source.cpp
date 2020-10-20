fb3034fb40d14d6f635668d2a1ed6b7085ce4047
Byankina <70693491+Byankina@users.noreply.github.com>
2020-10-21 01:23:24 +0300

    lab1 new

--- lr1/lr1/Source.cpp
+++ lr1/lr1/Source.cpp
@@ -10,8 +10,6 @@ struct truba {     //Создаем структуру труба со свойствами (как в примере задани
 	bool remont;
 };
 
-truba tr[10] = {};
-
 struct ks {        //Создаем структуру КС со свойствами (как в примере задания) 
 	int id;
 	string Name;
@@ -20,78 +18,69 @@ struct ks {        //Создаем структуру КС со свойствами (как в примере задания)
 	double effect;
 };
 
-bool check_value(int x) {
-	return x > 0 ? true : false;
+int get_int(int a) {
+	int i;
+	cin >> i;
+	while (i > a || i < 0 || cin.fail()) {
+		cout << "povtor " << endl;
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cin >> i;
+	}
+	cin.ignore(10000, '\n');
+	return i;
 }
-bool check_value(double x) {
-	return x > 0 ? true : false;
+
+double get_double(double a) {
+	double i;
+	cin >> i;
+	while (i > a || i < 0 || cin.fail()) {
+		cout << "povtor " << endl;
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cin >> i;
+	}
+	cin.ignore(10000, '\n');
+	return i;
 }
 
 truba create_truba() {                  //Создание нового объекта в структуре
 	truba new_truba;
-	do {
-		cin.clear();
-		cin.ignore(10000, '\n');
-		cout << "ID= " << endl;
-		cin >> new_truba.id;
-	} while (!check_value(new_truba.id) || cin.fail());
-	do {
-		cin.clear();
-		cin.ignore(10000, '\n');
-		cout << "Diameter= " << endl;
-		cin >> new_truba.d;
-	} while (!check_value(new_truba.d) || cin.fail());
-	do {
-		cin.clear();
-		cin.ignore(10000, '\n');
-		cout << "Dlina= " << endl;
-		cin >> new_truba.l;
-	} while (!check_value(new_truba.l) || cin.fail());
+	cout << "id=" << endl;
+	new_truba.id = get_int(1000);
+	cout << "diametr=" << endl;
+	new_truba.d = get_int(1000);
+	cout << "dlina=" << endl;
+	new_truba.l = get_int(1000);
 	new_truba.remont = false;
 	return new_truba;
 }
 ks create_ks() {     //Создание нового объекта в структуре КС
 	ks new_ks;
-	do {
-		cin.clear();
-		cin.ignore(10000, '\n');
-		cout << "ID= " << endl;
-		cin >> new_ks.id;
-	} while (!check_value(new_ks.id) || cin.fail());
 	cout << "Name: " << endl;
 	cin >> new_ks.Name;
-	do {
-		cin.clear();
-		cin.ignore(10000, '\n');
-		cout << "Kol-vo cehov= " << endl;
-		cin >> new_ks.kol_ceh;
-	} while (!check_value(new_ks.kol_ceh) || cin.fail());
-	do {
-		cin.clear();
-		cin.ignore(10000, '\n');
-		cout << "Kol-vo cehov inwork= " << endl;
-		cin >> new_ks.kol_ceh_inwork;
-	} while (!(check_value(new_ks.kol_ceh_inwork) && (new_ks.kol_ceh_inwork <= new_ks.kol_ceh)) || cin.fail());
-	do {
-		cin.clear();
-		cin.ignore(10000, '\n');
-		cout << "Effect =" << endl;
-		cin >> new_ks.effect;
-	} while (!check_value(new_ks.effect) || cin.fail());
+	cout << "id=" << endl;
+	new_ks.id = get_int(1000);
+	cout << "kol ceh=" << endl;
+	new_ks.kol_ceh = get_int(1000);
+	cout << "kol ceh inwork=" << endl;
+	new_ks.kol_ceh_inwork = get_int(new_ks.kol_ceh);
+	cout << "effect=" << endl;
+	new_ks.effect = get_int(1000);
 	return new_ks;
 }
 
-void write_truba_info(const truba& p, ks& comp) {     //Просмотр информации о трубе
-	cout << "TRUBA";
-	cout << "id: " << p.id << endl;
-	cout << "Diameter= " << p.d << endl;
-	cout << "Dlina= " << p.l << endl;
-	cout << (p.remont ? "V remonte" : "Ne v remonte") << endl;
-	cout << "KS";
-	cout << "ID: " << comp.id << endl;
-	cout << "Name: " << comp.Name << endl;
-	cout <<"in work "<< comp.kol_ceh-comp.kol_ceh_inwork  <<"/"<<comp.kol_ceh<< endl;
-	cout << "Effect= " << comp.effect << endl;
+void write_truba_ks_info(const truba& t, const ks& k) {     //Просмотр информации о трубе
+	cout << "TRUBA"<<endl;
+	cout << "id: " << t.id << endl;
+	cout << "Diameter= " << t.d << endl;
+	cout << "Dlina= " << t.l << endl;
+	cout << (t.remont ? "V remonte" : "Ne v remonte") << endl;
+	cout << "KS"<<endl;
+	cout << "ID: " << k.id << endl;
+	cout << "Name: " << k.Name << endl;
+	cout <<"in work "<< k.kol_ceh_inwork  <<"/"<<k.kol_ceh<< endl;
+	cout << "Effect= " << k.effect << endl;
 }
 
 void change_status(bool& status) {    //Изменить статус трубы
@@ -113,21 +102,20 @@ void save_to_file(truba p,ks c) {
 	}
 }
 
-truba load_from_file(truba& p,ks& comp) {
+void load_from_file(truba& t,ks& k) {
 	ifstream fin;
 	fin.open("lab.txt", ios::in);
 	if (fin.is_open()) {
-		fin >> p.id;
-		fin >> p.d;
-		fin >> p.l;
-		fin >> p.remont;
-		fin >> comp.Name;
-		fin >> comp.id;
-		fin >> comp.kol_ceh;
-		fin >> comp.kol_ceh_inwork;
-		fin >> comp.effect;
+		fin >> t.id;
+		fin >> t.d;
+		fin >> t.l;
+		fin >> t.remont;
+		fin >> k.Name;
+		fin >> k.id;
+		fin >> k.kol_ceh;
+		fin >> k.kol_ceh_inwork;
+		fin >> k.effect;
 		fin.close();
-		return p;
 	}
 }
 
@@ -145,8 +133,8 @@ void PrintMenu() {
 
 int main()
 {
-	ks comp;
-	truba p;
+	ks k;
+	truba t;
 	int i;
 	while (1) {
 		cout << "Select action:" << endl;
@@ -155,25 +143,25 @@ int main()
 			switch (i)
 			{
 			case 1:
-				p = create_truba();
+				t = create_truba();
 				break;
 			case 2:
-				comp = create_ks();
+				k = create_ks();
 				break;
 			case 3:
-				write_truba_info(p, comp);
+				write_truba_ks_info(t,k);
 				break;
 			case 4:
-				change_status(p.remont);
+				change_status(t.remont);
 				break;
 			case 5:
-				change_kol(comp);
+				change_kol(k);
 				break;
 			case 6:
-				save_to_file(p, comp);
+				save_to_file(t,k);
 				break;
 			case 7:
-				load_from_file(p, comp);
+				load_from_file(t,k);
 				break;
 			case 0:
 				return 0;
