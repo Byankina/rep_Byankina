3cb9dfd4fef15144acf6606dc52456383e4ca8c3
Byankina <70693491+Byankina@users.noreply.github.com>
2020-10-27 13:12:23 +0300

    lr1 gotovo
    Исправлено: 1. Проверка данных (изменена функция так, чтобы ее можно было применять под разные типы данных)   2. Разделены функции просмотр трубы и КС, загрузка их в файл и из файла     3. Если у нас нет трубы или КС, компилятор выведет, что у нас их нет    4. Имя КС можно вводить с пробелом

--- lr1/lr1/Source.cpp
+++ lr1/lr1/Source.cpp
@@ -18,19 +18,6 @@ struct ks {        //Создаем структуру КС со свойствами (как в примере задания)
 	double effect;
 };
 
-//int check_int(int a) {             //функция проверки значения
-//	int i;
-//	cin >> i;
-//	while (i > a || i < 0 || cin.fail()) {
-//		cout << "povtor " << endl;
-//		cin.clear();
-//		cin.ignore(10000, '\n');
-//		cin >> i;
-//	}
-//	//cin.ignore(10000, '\n');
-//	return i;
-//}
-
 template <typename T>
 T GetCorrectNumber(T max)
 {
@@ -59,7 +46,9 @@ truba create_truba() {                  //Создание нового объекта в структуре
 ks create_ks() {     //Создание нового объекта в структуре КС
 	ks new_ks;
 	cout << "Name: " << endl;
-	cin >> new_ks.Name;
+	cin.ignore(10000, '\n');
+	getline(cin, new_ks.Name);
+	//cin.getline (new_ks.Name);
 	cout << "id=" << endl;
 	new_ks.id = GetCorrectNumber(1000);
 	cout << "kol ceh=" << endl;
@@ -71,64 +60,91 @@ ks create_ks() {     //Создание нового объекта в структуре КС
 	return new_ks;
 }
 
-void write_truba_ks_info(const truba& t, const ks& k) {     //Просмотр информации о трубе
-	cout << "TRUBA"<<endl;
-	cout << "id: " << t.id << endl;
-	cout << "Diameter= " << t.d << endl;
-	cout << "Dlina= " << t.l << endl;
-	cout << (t.remont ? "V remonte" : "Ne v remonte") << endl;
-	cout << "KS"<<endl;
-	cout << "ID: " << k.id << endl;
-	cout << "Name: " << k.Name << endl;
-	cout <<"in work "<< k.kol_ceh_inwork  <<"/"<<k.kol_ceh<< endl;
-	cout << "Effect= " << k.effect << endl;
+void write_truba_info(const truba& t) {     //Просмотр информации о трубе
+	if (t.id > 0) { 
+		cout << "TRUBA" << endl;
+		cout << "id: " << t.id << endl;
+		cout << "Diameter= " << t.d << endl;
+		cout << "Dlina= " << t.l << endl;
+		cout << (t.remont ? "V remonte" : "Ne v remonte") << endl; 
+	}
+	else cout << "We have not the pipe"<< endl;
+}
+
+void write_ks_info(const ks& k) {     //Просмотр информации о КС
+	if (k.id > 0) {
+		cout << "KS" << endl;
+		cout << "ID: " << k.id << endl;
+		cout << "Name: " << k.Name << endl;
+		cout << "in work " << k.kol_ceh_inwork << "/" << k.kol_ceh << endl;
+		cout << "Effect= " << k.effect << endl;
+	}
+	else cout << "We have not the KS" << endl;
+
 }
 
 void change_status(bool& status) {    //Изменить статус трубы
 	status = !status;
 }
 
-void change_kol(ks& name) {       //
+void change_kol(ks& name) {           //Изменить количество цехов в работе
 	cout << "Kol-vo cehov inwork= " << endl;
 	name.kol_ceh_inwork = GetCorrectNumber(name.kol_ceh);
 }
 
 
-void save_to_file(truba p,ks c) {
+void save_to_file_pipe(const truba& p) {
 	ofstream fout;
-	fout.open("lab.txt", ios::out);
+	fout.open("Pipe.txt", ios::out);
 	if (fout.is_open()) {
-		fout << p.id << endl << p.d << endl << p.l << endl << p.remont <<endl << c.id << endl << c.Name << endl << c.kol_ceh << endl << c.kol_ceh_inwork << endl << c.effect;
+		fout << p.id << endl << p.d << endl << p.l << endl << p.remont <<endl;
 		fout.close();
 	}
 }
 
-void load_from_file(truba& t,ks& k) {
+void save_to_file_ks(const ks& c) {
+	ofstream fout;
+	fout.open("KS.txt", ios::out);
+	if (fout.is_open()) {
+		fout <<  c.id << endl << c.Name << endl << c.kol_ceh << endl << c.kol_ceh_inwork << endl << c.effect;
+		fout.close();
+	}
+}
+
+
+void load_from_file_pipe(truba& t) 
+{
 	ifstream fin;
-	fin.open("lab.txt", ios::in);
+	fin.open("Pipe.txt", ios::in);
 	if (fin.is_open()) {
-		fin >> t.id;
-		fin >> t.d;
-		fin >> t.l;
-		fin >> t.remont;
-		fin >> k.Name;
-		fin >> k.id;
-		fin >> k.kol_ceh;
-		fin >> k.kol_ceh_inwork;
-		fin >> k.effect;
+		fin >> t.id>> t.d>> t.l>> t.remont;
+		fin.close();
+	}
+}
+
+void load_from_file_ks(ks& k) 
+{
+	ifstream fin;
+	fin.open("KS.txt", ios::in);
+	if (fin.is_open()) {
+		fin >> k.Name>> k.id >> k.kol_ceh >> k.kol_ceh_inwork>> k.effect;
 		fin.close();
 	}
 }
 
 
 void PrintMenu() {
-	cout << "1. Dobavit' trubu" << endl;
-	cout << "2. Dobavit' KS" << endl;
-	cout << "3. Prosmotr truba+ks" << endl;
-	cout << "4. Redaktirovat' trubu(remont/ne remont)" << endl;
-	cout << "5. Redaktirovat' KS" << endl;
-	cout << "6. Sohranit' truba+ks" << endl;
-	cout << "7. Zagruzit' dannie trubi+ks iz faila" << endl;
+	cout << endl;
+	cout << "1. Add pipe" << endl;
+	cout << "2. Add KS" << endl;
+	cout << "3. Show the pipe" << endl;
+	cout << "4. Show the KS" << endl;
+	cout << "5. Change the pipe(remont/ne remont)" << endl;
+	cout << "6. Change the KS" << endl;
+	cout << "7. Save the pipe" << endl;
+	cout << "8. Save the KS" << endl;
+	cout << "9. Load from file the pipe" << endl;
+	cout << "10. Load from file the KS" << endl;
 	cout << "0. Exit" << endl;
 }
 
@@ -140,7 +156,7 @@ int main()
 	while (1) {
 		cout << "Select action:" << endl;
 		PrintMenu();
-		i = GetCorrectNumber(7);
+		i = GetCorrectNumber(10);
 			switch (i)
 			{
 			case 1:
@@ -150,27 +166,38 @@ int main()
 				k = create_ks();
 				break;
 			case 3:
-				write_truba_ks_info(t,k);
+				write_truba_info(t);
 				break;
 			case 4:
+				write_ks_info(k);
+			case 5:				
 				change_status(t.remont);
 				break;
-			case 5:
+			case 6:
 				change_kol(k);
 				break;
-			case 6:
-				save_to_file(t,k);
-				break;
 			case 7:
-				load_from_file(t,k);
+				save_to_file_pipe(t);
+				break;
+			case 8:
+				save_to_file_ks(k);
+			case 9:
+				load_from_file_pipe(t);
+				break;
+			case 10: 
+				load_from_file_ks(k);
 				break;
 			case 0:
 				return 0;
 				break;
-			default:
-				cout << "Vybor deystviya: " << endl;
-				break;
+			/*default:
+			{cout << endl;
+			cout << "Vybor deystviya: " << endl;}
+			break;*/
 			}
+
+			cout << endl;
+
 		
 
 	}
